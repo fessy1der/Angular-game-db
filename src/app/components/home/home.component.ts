151baae9fb7f49e325f90814ca5cae41016aeff5
Festus obi <fessy_wonder@yahoo.co.uk>
2021-06-21 11:40:50 +0200

    added sorting

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -25,7 +25,7 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  searchGames(sort: string, search?: string){
+  searchGames(sort: string, search?: string) : void{
     this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
       this.games = gameList.results;
       console.log(gameList);
